7c6e560957b37030214970a488712067bc626a5c
bramar2 <bramar224@gmail.com>
2025-03-29 01:19:45 +0700

    update jsoninput

--- input.cpp
+++ input.cpp
@@ -3,24 +3,25 @@
 
 namespace json {
 	JsonInput::JsonInput(std::istream* c_in) : in(c_in) {}
+
+	bool JsonInput::refill() {
+		if (ptr != available) return true;
+		in->read(buffer, BUFFER_SIZE);
+		ptr = 0;
+		available = in->gcount();
+		return available != 0;
+	}
+
 	char JsonInput::next() {
-		if (ptr == available) {
-			in->read(buffer, BUFFER_SIZE);
-			ptr = 0;
-			available = in->gcount();
-			if (available == 0) return '\0';
-		}
+		if (!refill()) return '\0';
 		return buffer[ptr++];
 	}
+
 	char JsonInput::peek() {
-		if (ptr == available) {
-			in->read(buffer, BUFFER_SIZE);
-			ptr = 0;
-			available = in->gcount();
-			if (available == 0) return '\0';
-		}
+		if (!refill()) return '\0';
 		return buffer[ptr];
 	}
+	
 	char JsonInput::next_iw() {
 		char res;
 		while (std::isspace( res = next() ));
--- input.hpp
+++ input.hpp
@@ -5,11 +5,13 @@
 namespace json {
 	class JsonInput {
 	public:
-		static constexpr size_t BUFFER_SIZE = 1024;
+		static constexpr size_t BUFFER_SIZE = 4096;
 		std::istream* in;
 		char buffer[BUFFER_SIZE];
 		size_t ptr = 0, available = 0;
-
+	private:
+		bool refill();
+	public:
 		JsonInput(std::istream* c_in);
 		char next();
 		char peek();
