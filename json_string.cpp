01c556a69454aba49ca37f2b7109af3d8676d455
bramar2 <bramar224@gmail.com>
2025-04-05 00:39:07 +0700

    add support for \uXXXX unicode chars and surrogate pairs

--- json_string.cpp
+++ json_string.cpp
@@ -8,7 +8,7 @@ namespace json {
 			case '\b': case '\t': case '\n': case '\f': case '\r':
 				break;
 			default:
-				if (c < ' ') {
+				if (0 < c && c < ' ') {
 					return false;
 				}
 				break;
@@ -71,7 +71,7 @@ namespace json {
 		char ch;
 		bool end = false;
 		while ( !end && (ch = in.next()) ) {
-			if (ch < ' ') {
+			if (0 < ch && ch < ' ') {
 				return false;
 			}
 			switch (ch) {
@@ -101,6 +101,71 @@ namespace json {
 					break;
 				case '"':
 					newValue.push_back('"');
+					break;
+				case 'u':
+					{
+						char hexCode[5];
+						hexCode[4] = '\0';
+						for (int i = 0; i < 4; i++) {
+							hexCode[i] = in.next();
+							if (std::isdigit(hexCode[i]) || ('a' <= hexCode[i] && hexCode[i] <= 'f') || 
+									('A' <= hexCode[i] && hexCode[i] <= 'F')) {
+								continue;
+							}
+							return false;
+						}
+						int codePoint = std::strtol(hexCode, NULL, 16);
+						if (codePoint <= 0x007F) {
+							newValue.push_back(char(codePoint));
+						} else if (codePoint <= 0x07FF) {
+							newValue.push_back(char(
+								0b11000000 | (codePoint >> 6)
+								));
+							newValue.push_back(char(
+								0b10000000 | (codePoint & 0x3F)
+								));
+						} else if (0xD800 <= codePoint && codePoint <= 0xDBFF) { // surrogate pair
+							if (in.next() != '\\' || in.next() != 'u') {
+								return false;
+							}
+							for (int i = 0; i < 4; i++) {
+								hexCode[i] = in.next();
+								if (std::isdigit(hexCode[i]) || ('a' <= hexCode[i] && hexCode[i] <= 'f') || 
+										('A' <= hexCode[i] && hexCode[i] <= 'F')) {
+									continue;
+								}
+								return false;
+							}
+							int lowCodePoint = std::strtol(hexCode, NULL, 16);
+							if (!(0xDC00 <= lowCodePoint && lowCodePoint <= 0xDFFF)) {
+								return false;
+							}
+							int high = codePoint & 0x3FF, low = lowCodePoint & 0x3FF;
+							codePoint = 0x10000 + ((high << 10) | low);
+							newValue.push_back(char(
+								0b11110000 | (codePoint >> 18)
+								));
+							newValue.push_back(char(
+								0b10000000 | ((codePoint >> 12) & 0x3F)
+								));
+							newValue.push_back(char(
+								0b10000000 | ((codePoint >> 6) & 0x3F)
+								));
+							newValue.push_back(char(
+								0b10000000 | (codePoint & 0x3F)
+								));
+						} else {
+							newValue.push_back(char(
+								0b11100000 | (codePoint >> 12)
+								));
+							newValue.push_back(char(
+								0b10000000 | ((codePoint >> 6) & 0x3F)
+								));
+							newValue.push_back(char(
+								0b10000000 | (codePoint & 0x3F)
+								));
+						}
+					}
 					break;
 				default:
 					return false;
